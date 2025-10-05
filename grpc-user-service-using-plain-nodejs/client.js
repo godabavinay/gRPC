f4e3a2e39867e58bb5c0dd66383eda1c96dea947
Vinay Godaba <godabavinay@gmail.com>
2025-10-05 13:58:53 +0530

    init: gRPC server & client using nodejs

---
+++ grpc-user-service-using-plain-nodejs/client.js
@@ -0,0 +1,24 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+
+const packageDef = protoLoader.loadSync("user.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const userPackage = grpcObject.user;
+
+const client = new userPackage.UserService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+client.CreateUser(
+  { name: "Alice", email: "alice@example.com" },
+  (err, response) => {
+    if (err) return console.error(err);
+    console.log("Created User: ", response);
+
+    client.GetUser({ id: response.id }, (err2, user) => {
+      if (err2) return console.error(err2);
+      console.log("Fetched User: ", user);
+    });
+  }
+);
---
+++ grpc-user-service-using-plain-nodejs/server.js
@@ -0,0 +1,45 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import { v4 as uuidv4 } from "uuid";
+
+const packageDef = protoLoader.loadSync("user.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const userPackage = grpcObject.user;
+
+const users = new Map();
+const server = new grpc.Server();
+
+server.addService(userPackage.UserService.service, {
+  CreateUser: (call, callback) => {
+    const { name, email } = call.request;
+    const id = uuidv4();
+    const user = { id, name, email };
+    users.set(id, user);
+    callback(null, user);
+  },
+  GetUser: (call, callback) => {
+    const { id } = call.request;
+    const user = users.get(id);
+
+    if (!user) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        details: "User not found",
+      });
+    }
+
+    callback(null, user);
+  },
+});
+
+server.bindAsync(
+  "0.0.0.0:50051",
+  grpc.ServerCredentials.createInsecure(),
+  (err, port) => {
+    if (err) {
+      console.error("Server bind failed:", err);
+      return;
+    }
+    console.log(`gRPC server running on port ${port}`);
+  }
+);
